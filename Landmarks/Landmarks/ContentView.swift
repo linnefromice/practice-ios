c4f2701c61f1d2d82ea8525813d7fef1d838b07f
linnefromice <linnefromice@gmail.com>
2024-10-24 17:16:22 +0900

    initialize

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by arata.haruyama on 2024/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
