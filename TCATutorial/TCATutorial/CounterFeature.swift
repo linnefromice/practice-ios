4e4decaf2ae131dd8433a04c1dcffd87614d8d76
linnefromice <linnefromice@gmail.com>
2024-10-28 11:59:43 +0900

    update: chapter 2 - add sideeffect

--- TCATutorial/TCATutorial/CounterFeature.swift
+++ TCATutorial/TCATutorial/CounterFeature.swift
@@ -11,11 +11,14 @@ import ComposableArchitecture
 struct CounterFeature {
     @ObservableState
     struct State {
-        var count = 0;
+        var count = 0
+        var fact: String?
+        var isLoading = false
     }
     
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
     
@@ -24,9 +27,15 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
             }
         }
