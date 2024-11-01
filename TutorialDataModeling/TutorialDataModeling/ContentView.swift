fe77b44b0d8be94c431788c6fd90a278ca211deb
linnefromice <linnefromice@gmail.com>
2024-11-02 00:12:07 +0900

    update: chapter 1 > section 2

--- TutorialDataModeling/TutorialDataModeling/ContentView.swift
+++ TutorialDataModeling/TutorialDataModeling/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
+    @State private var scores: [Int] = [0, 0, 0]
     
     var body: some View {
         VStack {
             ForEach(0..<players.count, id: \.description) { index in
                 TextField("Name", text: $players[index])
+                Stepper("\(scores[index])", value: $scores[index])
             }
             Button("Add Player", systemImage: "plus") {
                 players.append("")
+                scores.append(0)
             }
         }
         .padding()
