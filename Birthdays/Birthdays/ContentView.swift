160e3dbd2771c5f74d5230e32f051c236fd027a0
linnefromice <linnefromice@gmail.com>
2024-11-03 00:19:04 +0900

    update: swift data - chapter 1 > section 6

--- Birthdays/Birthdays/ContentView.swift
+++ Birthdays/Birthdays/ContentView.swift
@@ -6,21 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        Friend(name: "Elton Lin", birthday: .now),
-        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
-    ]
+    @Query(sort: \Friend.birthday) private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     
     @State private var newName = ""
     @State private var newDate = Date.now
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack {
+                    if friend.isBirthdayToday {
+                        Image(systemName: "birthday.cake")
+                    }
                     Text(friend.name)
+                        .bold(friend.isBirthdayToday)
                     Spacer()
                     Text(friend.birthday, format: .dateTime.month(.wide).day().year())
                 }
@@ -37,16 +40,21 @@ struct ContentView: View {
                 }
                 Button("Save") {
                     let newFriend = Friend(name: newName, birthday: newDate)
-                    friends.append(newFriend)
+                    context.insert(newFriend)
                     newName = ""
                     newDate = .now
                 }
                 .bold()
             }
         }
+        .task {
+            context.insert(Friend(name: "Elton Lin", birthday: .now))
+            context.insert(Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)))
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
