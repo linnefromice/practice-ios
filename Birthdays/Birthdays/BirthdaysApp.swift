f2d59df78080b4227f7d1cdf2ffddca8ffa3063c
linnefromice <linnefromice@gmail.com>
2024-11-02 23:33:03 +0900

    initialize: swift data tutorial

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by arata.haruyama on 2024/11/02.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
