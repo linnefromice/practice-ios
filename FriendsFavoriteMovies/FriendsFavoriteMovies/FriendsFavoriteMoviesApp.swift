faa823eb5bf9dc3179c50a5ada6099b3d40c809d
linnefromice <linnefromice@gmail.com>
2024-11-04 01:23:32 +0900

    initialize

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by arata.haruyama on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
