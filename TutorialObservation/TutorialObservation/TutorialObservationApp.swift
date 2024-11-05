224e82b72436a216b098c7c6b955c7bd6f85ba7b
linnefromice <linnefromice@gmail.com>
2024-11-06 01:00:23 +0900

    initialize

---
+++ TutorialObservation/TutorialObservation/TutorialObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialObservationApp.swift
+//  TutorialObservation
+//
+//  Created by arata.haruyama on 2024/11/06.
+//
+
+import SwiftUI
+
+@main
+struct TutorialObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
