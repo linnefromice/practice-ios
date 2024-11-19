2eba19ca58d3208aadb1c61fd3d8ee28b49d90d1
linnefromice <linnefromice@gmail.com>
2024-11-19 16:18:19 +0900

    update

--- practice-multi-module/CounterAppModules/App/CounterApp/CounterAppApp.swift
+++ practice-multi-module/CounterAppModules/App/CounterApp/CounterAppApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Counter
 
 @main
 struct CounterAppApp: App {
--- practice-multi-module/CounterAppModules/Sources/Counter/CounterScreen.swift
+++ practice-multi-module/CounterAppModules/Sources/Counter/CounterScreen.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+final class CounterViewModel: ObservableObject {
+    @Published private(set) var count: Int = 0
+    
+    func increment() {
+        count += 1
+    }
+}
+
+public struct CounterScreen: View {
+    @StateObject private var viewModel: CounterViewModel = .init()
+    
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Text("\(viewModel.count)")
+                .monospacedDigit()
+            Button("+") {
+                viewModel.increment()
+            }
+        }
+        .font(.largeTitle)
+    }
+}
+
+#Preview {
+    CounterScreen()
+}
