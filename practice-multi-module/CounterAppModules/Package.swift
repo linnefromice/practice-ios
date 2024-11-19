0a4cb27c9b0e21a7d58e1d60b0c13020b0f721ef
linnefromice <linnefromice@gmail.com>
2024-11-19 15:55:25 +0900

    update: rename module

--- practice-multi-module/CounterAppModules/Package.swift
+++ practice-multi-module/CounterAppModules/Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ExampleModule",
-            targets: ["ExampleModule"]),
+            name: "Counter",
+            targets: ["Counter"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ExampleModule"),
+            name: "Counter"),
         .testTarget(
-            name: "ExampleModuleTests",
-            dependencies: ["ExampleModule"]
+            name: "CounterTests",
+            dependencies: ["Counter"]
         ),
     ]
 )
