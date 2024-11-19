cac4ae4c76cac40d441c2d8c2fa100956f5de405
linnefromice <linnefromice@gmail.com>
2024-11-19 13:28:42 +0900

    update: implement counter

--- practice-multi-module/CounterApp/CounterApp/CounterAppApp.swift
+++ practice-multi-module/CounterApp/CounterApp/CounterAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  CounterAppApp.swift
-//  CounterApp
-//
-//  Created by arata.haruyama on 2024/11/19.
-//
-
 import SwiftUI
 
 @main
 struct CounterAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterScreen()
         }
     }
 }
