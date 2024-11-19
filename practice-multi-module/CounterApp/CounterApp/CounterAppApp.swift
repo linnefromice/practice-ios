cc6007abf18a1b38aaea329e405dbb02d2dc186c
linnefromice <linnefromice@gmail.com>
2024-11-19 13:18:24 +0900

    initialize

---
+++ practice-multi-module/CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by arata.haruyama on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
