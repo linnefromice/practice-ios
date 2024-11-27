807d96726c1bb257fdeea3df9ecb0374cfae996e
linnefromice <linnefromice@gmail.com>
2024-11-27 17:12:06 +0900

    initialize

---
+++ practice-errors/GlobalErrorApp/GlobalErrorApp/GlobalErrorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalErrorAppApp.swift
+//  GlobalErrorApp
+//
+//  Created by arata.haruyama on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct GlobalErrorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
