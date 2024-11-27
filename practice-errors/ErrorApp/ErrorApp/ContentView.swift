b8fe82f1fe54d4c1b0fc8ad9641e62ab50f140f3
linnefromice <linnefromice@gmail.com>
2024-11-27 14:25:04 +0900

    initialize

---
+++ practice-errors/ErrorApp/ErrorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ErrorApp
+//
+//  Created by arata.haruyama on 2024/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
