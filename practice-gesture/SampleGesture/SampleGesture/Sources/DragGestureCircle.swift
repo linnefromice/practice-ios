7171faaae3147ca86f7b4924cdeaa9ba0c0d666e
linnefromice <linnefromice@gmail.com>
2025-01-14 17:01:40 +0900

    add: gesture sample - draggable

--- practice-gesture/SampleGesture/SampleGesture/Sources/DragGestureCircle.swift
+++ practice-gesture/SampleGesture/SampleGesture/Sources/DragGestureCircle.swift
@@ -1,22 +1,35 @@
 import SwiftUI
 
 public struct DragGestureCircle: View {
-    @State private var isDragging = false
+  @State private var isDragging = false
+  @State private var offsetX: CGFloat = 0
+  @State private var offsetY: CGFloat = 0
 
-    var drag: some Gesture {
-        DragGesture()
-            .onChanged { _ in self.isDragging = true }
-            .onEnded { _ in self.isDragging = false }
-    }
+  var drag: some Gesture {
+    DragGesture()
+      .onChanged { value in
+        self.isDragging = true
+        self.offsetX = value.translation.width
+        self.offsetY = value.translation.height
+      }
+      .onEnded { _ in
+        self.isDragging = false
+        // self.offsetX = 0
+        // self.offsetY = 0
+      }
+  }
 
-    public var body: some View {
-        Circle()
-            .fill(isDragging ? .red : .blue)
-            .frame(width: 100, height: 100)
-            .gesture(drag)
+  public var body: some View {
+    ZStack {
+      Circle()
+        .fill(isDragging ? .red : .blue)
+        .frame(width: 100, height: 100)
+        .offset(x: offsetX, y: offsetY)
+        .gesture(drag)
     }
+  }
 }
 
 #Preview {
-    DragGestureCircle()
+  DragGestureCircle()
 }
