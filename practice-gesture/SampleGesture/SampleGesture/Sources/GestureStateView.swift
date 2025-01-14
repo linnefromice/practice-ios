bc8e6387bdd7275c5ecf9fbb4d5747e477f3d730
linnefromice <linnefromice@gmail.com>
2025-01-14 17:14:12 +0900

    add: gesture sample - long press

---
+++ practice-gesture/SampleGesture/SampleGesture/Sources/GestureStateView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+public struct GestureStateView: View {
+    @GestureState private var isDetectingLongPress = false
+
+    var longPress: some Gesture {
+        LongPressGesture(minimumDuration: 2.0)
+            .updating($isDetectingLongPress) { currentState, gestureState, transaction in
+                gestureState = currentState
+            }
+    }
+
+    public var body: some View {
+        Circle()
+            .fill(self.isDetectingLongPress ? .red : .green)
+            .frame(width: 100, height: 100)
+            .gesture(longPress)
+    }
+}
+
+
+#Preview {
+    GestureStateView()
+}
