7d8928ce05ff358eb4c5d79a03f7f276d07283de
linnefromice <linnefromice@gmail.com>
2025-01-16 17:13:05 +0900

    initialize

---
+++ practice-rive/SampleRive/SampleRive/SampleRiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleRiveApp.swift
+//  SampleRive
+//
+//  Created by arata.haruyama on 2025/01/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleRiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
