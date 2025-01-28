2be5039c4ec85ef2c49e0113b2de9bb7e04a0a18
linnefromice <linnefromice@gmail.com>
2025-01-28 17:23:27 +0900

    initialize

---
+++ practice-gesture/SampleSwipe/SampleSwipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleSwipe
+//
+//  Created by arata.haruyama on 2025/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practice-gesture/SampleSwipe/SampleSwipe/SampleSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwipeApp.swift
+//  SampleSwipe
+//
+//  Created by arata.haruyama on 2025/01/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
