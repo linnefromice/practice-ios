c23f8856080cc7bd57591bc43ca07d99e569fcea
linnefromice <linnefromice@gmail.com>
2025-01-31 17:15:03 +0900

    add: sample app

---
+++ TemplateSPM/Apps/MainApp/MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by arata.haruyama on 2025/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
