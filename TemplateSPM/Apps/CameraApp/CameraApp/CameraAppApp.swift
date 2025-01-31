0b9f6cdb997499b4f57e97d6e9922e1a2acae35c
linnefromice <linnefromice@gmail.com>
2025-01-31 19:11:42 +0900

    add: app for camera

---
+++ TemplateSPM/Apps/CameraApp/CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by arata.haruyama on 2025/01/31.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
