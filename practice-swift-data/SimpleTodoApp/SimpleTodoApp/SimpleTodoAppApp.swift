fd2951e410f69a35865ec3f3e85779d667b9a472
linnefromice <linnefromice@gmail.com>
2025-02-27 09:53:59 +0900

    initialize

---
+++ practice-swift-data/SimpleTodoApp/SimpleTodoApp/SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoAppApp.swift
+//  SimpleTodoApp
+//
+//  Created by arata.haruyama on 2025/02/27.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
