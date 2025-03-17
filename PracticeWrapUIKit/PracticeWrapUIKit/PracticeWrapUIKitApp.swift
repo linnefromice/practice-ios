ce0f4873c1c7585594632ecf433b2760aa82dfe2
linnefromice <linnefromice@gmail.com>
2025-03-17 12:47:19 +0900

    initialize

---
+++ PracticeWrapUIKit/PracticeWrapUIKit/PracticeWrapUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeWrapUIKitApp.swift
+//  PracticeWrapUIKit
+//
+//  Created by arata.haruyama on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct PracticeWrapUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
