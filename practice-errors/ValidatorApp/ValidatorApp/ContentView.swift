2ec70996fe824f97fbf244490d346295d70a6d83
linnefromice <linnefromice@gmail.com>
2025-04-05 23:39:04 +0900

    initialize

---
+++ practice-errors/ValidatorApp/ValidatorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ValidatorApp
+//
+//  Created by arata.haruyama on 2025/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practice-errors/ValidatorApp/ValidatorApp/ValidatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatorAppApp.swift
+//  ValidatorApp
+//
+//  Created by arata.haruyama on 2025/04/05.
+//
+
+import SwiftUI
+
+@main
+struct ValidatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
