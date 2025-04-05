28c541841fb0e1dd6a5b7369db5d3cf46679e40e
linnefromice <linnefromice@gmail.com>
2025-04-06 00:13:42 +0900

    add: test targets

---
+++ practice-errors/ValidatorApp/ValidatorAppTests/ValidatorAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidatorAppTests.swift
+//  ValidatorAppTests
+//
+//  Created by arata.haruyama on 2025/04/06.
+//
+
+import Testing
+
+struct ValidatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
